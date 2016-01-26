1912ce2e3448e62afa4c865e40eeee4286f4bbec
Andy <acarl005@g.ucla.edu>
2016-01-26 10:48:43 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Google Tab Hotkey
+================
+
+I use Google search very heavily.
+It would save a lot of time if I didnt have to grab the mouse to click the link I want.
+The default TAB behavior gets you nowhere fast.
+I'd rather scroll through each link by pressing TAB and shift+TAB.
+This Chrome extension adds that functionality.
+
---
+++ code/js.js
@@ -0,0 +1,37 @@
+const TAB_KEY = 9
+
+let searchResults
+let activeIndex = -1
+
+$(document).ready(() => {
+  $(document).on('keydown', e => {
+    // do nothing if this isn't the TAB key being pressed
+    if (e.keyCode !== TAB_KEY) {
+      return
+    }
+
+    e.preventDefault()
+
+    // get the search result items from the DOM if we haven't already
+    searchResults = searchResults || $('._NId > .srg > .g, ._NId > .g:not(.g-blk)')
+
+    // see if we are going forward or backward
+    if (e.shiftKey) {
+      activeIndex = Math.max(activeIndex - 1, -1)
+    } else {
+      activeIndex = Math.min(activeIndex + 1, searchResults.length - 1)
+    }
+
+    // find the next search result to focus
+    // focus the search query input bar if we go all the way back to the very beginning
+    if (activeIndex === -1) {
+      const queryInput = $('#lst-ib')
+      queryInput.focus()
+    } else {
+      const targetElement = searchResults[activeIndex]
+      const targetLink = $(targetElement).find('.rc > .r > a')
+      targetLink.focus()
+    }
+  })
+})
+
